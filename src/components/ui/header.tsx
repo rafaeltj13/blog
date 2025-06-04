f2607b74df847c8534142d3967aadc9f6f91e983
Rafael Maciel <rmaciel-contractor@studylog.com>
2025-06-04 09:51:14 -0300

    updated

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -23,16 +23,14 @@ export default function Header() {
       const isDarkMode = window.matchMedia(
         "(prefers-color-scheme: dark)"
       ).matches;
-      console.log({ isDarkMode });
       setTheme(isDarkMode ? "dark" : "light");
     }
-  }, []);
+  }, [setTheme]);
 
   return (
     <header
-      className={`flex items-center justify-between py-4 fixed top-0 left-0 right-0 z-50 w-full backdrop-blur-sm max-w-screen-xl mx-auto transition-all duration-300 ${
-        isScrolled ? "bg-background/50 border-primary" : ""
-      }`}
+      className={`flex items-center justify-between py-4 fixed top-0 left-0 right-0 z-50 w-full backdrop-blur-sm max-w-screen-xl mx-auto transition-all duration-300 ${isScrolled ? "bg-background/50 border-primary" : ""
+        }`}
     >
       <div className="flex items-center pl-8 w-64">
         <Link href="/" className="text-2xl font-bold">
